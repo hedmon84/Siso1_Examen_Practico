f7c516a5fe218588e93facec35de0ca25c690f93
Hedmon Lopez <hedmonlopes@gmail.com>
2020-11-21 15:03:24 -0600

    Add files via upload

--- eje2.c
+++ eje2.c
@@ -22,42 +22,34 @@ int main(int argc, char *argv[])
 
     pthread_t tList[NUM_THREADS];
     struct thread_data tData[NUM_THREADS];
-
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_attr_t attr[NUM_THREADS];
 
     sem_init(&sem[0], 0, 1);
-    sem_init(&sem[1], 0, 1);
-    sem_init(&sem[2], 0, 1);
-
-    tData[0].timeup = 0;
-    tData[1].timeup = 2;
-    tData[2].timeup = 1;
+    sem_init(&sem[1], 0, 0);
+    sem_init(&sem[2], 0, 0);
 
     int i;
     for (i = 0; i < NUM_THREADS; i++)
     {
+        pthread_attr_init(&attr[i]);
         tData[i].id = i;
-        pthread_create(&tList[i], &attr, runner, &tData[i]);
+        pthread_create(&tList[i], &attr[i], runner, &tData[i]);
     }
 
     for (i = 0; i < NUM_THREADS; i++)
         pthread_join(tList[i], NULL);
-    pthread_attr_destroy(&attr);
-    pthread_exit(NULL);
-
     for (i = 0; i < NUM_THREADS; i++)
         sem_destroy(&sem[i]);
+    printf("------ Press Any key to continue ------\n");
 
     return 0;
 }
 
-void *runner(void *param)
+void *runner(void *in)
 {
     int i;
     struct thread_data *dat;
-    dat = (struct thread_data *)param;
+    dat = (struct thread_data *)in;
 
     sem_wait(&sem[dat->id]);
 
@@ -65,7 +57,14 @@ void *runner(void *param)
     usleep(6000);
     printf("Proceso:%d en seccion restante\n", dat->id);
 
-    sem_post(&sem[dat->timeup]);
+    if (dat->id == 0)
+    {
+        sem_post(&sem[2]);
+    }
+    else
+    {
+        sem_post(&sem[1]);
+    }
 
     pthread_exit(0);
 }
